17f4bc32159c73aaf3c6642554f09365c9fea5cd
Aditya Prakash <leibton@gatech.edu>
2024-12-13 04:05:24 -0500

    feature test 1

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,10 +1,18 @@
-import React, {useState} from "react"
-
+import React, {useState, useEffect} from "react"
+import { baseUrl } from "../shared/baseUrl"
 import Mainbody from "./Mainbody";
 import PopularPart from "./PopularPart";
 import Footer from "./Footer";
 function Homecontent(){
 document.title = "Home | Project Ekatra"
+
+useEffect(() => {
+  // Send request to backend on page load
+  fetch(baseUrl+"api/load")
+  .then(response => {console.log("Page Loaded Successfully")})
+    .catch((error) => console.error("Error:", error));
+}, []);
+
   return <div>
     <Mainbody />
   <PopularPart />
